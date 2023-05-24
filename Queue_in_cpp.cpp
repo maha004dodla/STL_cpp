2a3a2c9c925795f695cc7f36a8d2f799443d0538
maha <97425755+maha004dodla@users.noreply.github.com>
2023-05-24 09:15:58 +0530

    Queue_in_cpp.cpp

--- Queue_in_cpp.cpp
+++ Queue_in_cpp.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 void display(queue<int> p)
 {
+	cout<<"dequeing elements"<<endl;
 	queue<int> temp=p;
 	while(!temp.empty())
 	{
@@ -27,6 +28,9 @@ int main()
 		cin>>b;
 		q2.push(b);
 	}
+	cout <<"The size of the queue is : "<<q.size()<<endl; //size() function 
+	cout<<"The first element that entered the queue is : "<<q.front()<<endl;  //front() function
+	cout<<"The last element that entered the queue is : "<<q.back()<<endl;    //back() function
 	display(q1);
 	display(q2);
 }
