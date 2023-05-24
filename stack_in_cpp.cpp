da747b64c7be020d6ff76200e1ac7619dff84412
maha <97425755+maha004dodla@users.noreply.github.com>
2023-05-24 09:06:23 +0530

    Add files via upload

---
+++ stack_in_cpp.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void display(stack<int> q)
+{
+	stack<int>p=q;
+	while(!p.empty())
+	{
+		cout<<p.top()<<" ";
+		p.pop();
+	}
+	cout<<endl;
+}
+int main()
+{
+	int n,a,i;
+	cin>>n;
+	stack<int>q;
+	for(i=0; i<n; i++)
+	{
+		cin>>a;
+		q.push(a);
+	}
+	display(q);
+}
+/*ip 1 1 2 2 2 3
+op 3 2 2 2 1 1*/
