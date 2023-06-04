1d1aa2c52d606e0c7dc5caabdcf09f2c960bee60
maha <97425755+maha004dodla@users.noreply.github.com>
2023-06-04 21:06:26 +0530

    reverse_string_with_spaces.cpp

--- reverse_string_with_spaces.cpp
+++ reverse_string_with_spaces.cpp
@@ -4,30 +4,30 @@ int main()
 {
 	string s;
 	getline(cin,s);//to read input with spaces
-    stack<string>st;
-    string res="",ans="";
+        stack<string>st;
+        string res="",ans="";
 	for(int i=0; i<s.size(); i++)
-    {
-		ans=" ";
+        {
+	    ans=" ";
 	    if(s[i]==' ')
 	    {    
 	    	continue;
-        }
-        while(s[i]!=' ' and i<s.size())//to insert elements one by one until there exixts space 
-        //maha(pop)--the(pop)---great(pop);
-        {
+            }
+            while(s[i]!=' ' and i<s.size())//to insert elements one by one until there exixts space 
+            //maha(pop)--the(pop)---great(pop);
+            {
             ans+=s[i];//m+a+h+a...
             i++;
+            }
+            st.push(ans);//maha--the--great
         }
-        st.push(ans);//maha--the--great
-    }
-    while(!st.empty())
-    {
-		res+=st.top();
-        st.pop();
-    }
-    res.erase(0,1);//to remove space in given range
-    cout<<res;
+        while(!st.empty())
+   	{
+           res+=st.top();
+           st.pop();
+        }
+        res.erase(0,1);//to remove space in given range
+        cout<<res;
 }
 
 /*inp: maha is great
