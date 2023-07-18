6db5a6b591fd860998e5f7b2c745c6e1bea79b13
maha <97425755+maha004dodla@users.noreply.github.com>
2023-07-18 09:56:49 +0530

    DEMO_vector_linkedlist.cpp

--- DEMO_vector_linkedlist.cpp
+++ DEMO_vector_linkedlist.cpp
@@ -1,5 +1,6 @@
     ListNode* vec_ll(vector<int>&res)
     {
+        //method1:
         ListNode *head=NULL;
         ListNode *tail=NULL;
         for(auto i:res)
@@ -19,4 +20,23 @@
             }
         }
         return head;
+
+        /*method 2:
+        Node *head1=NULL;
+        Node *tail=head1;
+        for(int i=0; i<l.size(); i++)
+        {
+            Node *nn=new Node(l[i]);
+            if(head1==NULL)
+            {
+                head1=nn;
+                tail=head1;
+            }
+            else
+            {
+                tail->next=nn;
+                tail=tail->next;
+            }
+        }
+        return head1;*/
     }
